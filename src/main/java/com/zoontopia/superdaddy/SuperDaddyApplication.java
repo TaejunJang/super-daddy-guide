abc040569294eecf573037e81b7ec1c045d8185d
jtj <rigun300@naver.com>
2025-12-26 14:18:32 +0900

    init

---
+++ src/main/java/com/zoontopia/superdaddy/SuperDaddyApplication.java
@@ -0,0 +1,13 @@
+package com.zoontopia.superdaddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperDaddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperDaddyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zoontopia/superdaddy/SuperDaddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoontopia.superdaddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperDaddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
